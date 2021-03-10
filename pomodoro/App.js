9cd592e6c65362b63addacb89bf1910d72933da4
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-03-09 21:19:06 -0300

    Botões finalizados.

--- pomodoro/App.js
+++ pomodoro/App.js
@@ -4,13 +4,13 @@ import { StyleSheet, Text, View, Vibration } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler'
 import Slider from '@react-native-community/slider'
 
-// Vibration.vibrate([500, 500, 500])
 let countdownTimeout
 
 export default function App() {
-  const [workTime, setWorkTime] = useState(0.10 * 60)
-  const [restTime, setRestTime] = useState(0.05 * 60)
-  const [currentTime, setCurrentTime] = useState(0.10 * 60)
+  const [workTime, setWorkTime] = useState(5 * 60)
+  const [restTime, setRestTime] = useState(1 * 60)
+  const [currentTime, setCurrentTime] = useState(5 * 60)
+  const [isHome, setIsHome] = useState(true)
   const [isRest, setIsRest] = useState(false)
   const [isRunning, setIsRunning] = useState(false)
 
@@ -24,6 +24,7 @@ export default function App() {
     setCurrentTime(workTime)
     setIsRest(false)
     setIsRunning(true)
+    setIsHome(false)
   }
 
   function handlePause() {
@@ -31,10 +32,15 @@ export default function App() {
     setIsRunning(false)
   }
 
+  function handleComeback() {
+    setIsRunning(true)
+  }
+
   function handleReset() {
     clearTimeout(countdownTimeout)
     setIsRunning(false)
     setCurrentTime(workTime)
+    setIsHome(true)
   }
 
   useEffect(() => {
@@ -43,12 +49,18 @@ export default function App() {
         setCurrentTime(currentTime - 1)
       }, 1000) 
     } else if (isRunning && !isRest && currentTime === 0) {
+      Vibration.vibrate([500, 500, 500])
+      clearTimeout(countdownTimeout)
       setIsRest(true)
+
       setTimeout(() => {
         setCurrentTime(restTime)
       }, 1000)
     } else if (isRunning && isRest && currentTime === 0) {
+      Vibration.vibrate([500, 500, 500])
+      clearTimeout(countdownTimeout)
       setIsRest(false)
+
       setTimeout(() => {
         setCurrentTime(workTime)
       }, 1000)
@@ -65,45 +77,56 @@ export default function App() {
         </Text>
       </View>
 
-      <View>
+      <View style={{alignItems: 'center'}}>
+        <Text style={styles.label}>Tempo de Trabalho</Text>
         <View style={styles.range}>
           <Slider
             style={{width: 270, height: 33}}
-            minimumValue={1} // <----- 5
+            minimumValue={5}
             maximumValue={50}
-            thumbTintColor="#09f"
-            minimumTrackTintColor="#09f"
+            thumbTintColor="#03A9F4"
+            minimumTrackTintColor="#03A9F4"
             step={5}
             onValueChange={(value) => setWorkTime(value * 60)}
           />
-          <Text>{workTime} min</Text>
+          <Text style={styles.timeLabel}>{String(Math.round(workTime / 60)).padStart(2, '0')} min</Text>
         </View>
 
+        <Text style={styles.label}>Tempo de Descanço</Text>
         <View style={styles.range}>
           <Slider
             style={{width: 270, height: 33}}
             minimumValue={1}
             maximumValue={10}
-            thumbTintColor="#fa0"
-            minimumTrackTintColor="#fa0"
+            thumbTintColor="#FFB74D"
+            minimumTrackTintColor="#FFB74D"
             step={1}
-            onValueChange={(value) => setRestTime(value * 60)}
+            onValueChange={(value) => setRestTime((value) * 60)}
           />
-          <Text>{restTime} min</Text>
+          <Text style={styles.timeLabel}>{String(Math.round(restTime / 60)).padStart(2, '0')} min</Text>
         </View>
       </View>
 
-      <View style={styles.buttonGroup}>
+      { isHome ? (
         <RectButton style={[styles.button, styles.start]} onPress={handleStart}>
           <Text style={styles.buttonText}>Iniciar</Text>
         </RectButton>
-        <RectButton style={[styles.button, styles.pause]} onPress={handlePause}>
-          <Text style={styles.buttonText}>Parar</Text>
-        </RectButton>
-        <RectButton style={[styles.button, styles.reset]} onPress={handleReset}>
-          <Text style={styles.buttonText}>Reiniciar</Text>
-        </RectButton>
-      </View>
+      ) : (
+        <View style={styles.buttonGroup}>
+          { isRunning ? (
+            <RectButton style={[styles.button, styles.pause]} onPress={handlePause}>
+              <Text style={styles.buttonText}>Parar</Text>
+            </RectButton>
+          ) : (
+            <RectButton style={[styles.button, styles.pause]} onPress={handleComeback}>
+              <Text style={styles.buttonText}>Retomar</Text>
+            </RectButton>
+          ) }
+          <RectButton style={[styles.button, styles.reset]} onPress={handleReset}>
+            <Text style={styles.buttonText}>Reiniciar</Text>
+          </RectButton>
+        </View>
+      ) }
     </View>
   );
 }
@@ -132,10 +155,21 @@ const styles = StyleSheet.create({
     fontSize: 80,
   },
 
+  label: {
+    fontSize: 18,
+    marginVertical: 4
+  },
+
   range: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+
+  timeLabel: {
+    backgroundColor: '#fff',
+    padding: 4,
+    borderRadius: 5
   },
 
   buttonGroup: {
@@ -144,30 +178,36 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     maxHeight: 60,
-    width: 300
+    width: '90%'
   },
 
   button: {
-    height: 40,
-    width: 80,
-    borderRadius: 8,
+    height: 60,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    bottom: -50
   },
 
   start: {
-    backgroundColor: '#7e0'    
+    backgroundColor: '#64DD17',
+    width: '50%',
+    borderRadius: 8 
   },
 
   pause: {
-    backgroundColor: '#f42'
+    backgroundColor: '#f44336',
+    width: '45%',
+    borderRadius: 8
   },
 
   reset: {
-    backgroundColor: '#57f'
+    backgroundColor: '#1E88E5',
+    width: '45%',
+    borderRadius: 8
   },
 
   buttonText: {
-    color: '#fff'
+    color: '#fff',
+    fontSize: 22
   }
 });
